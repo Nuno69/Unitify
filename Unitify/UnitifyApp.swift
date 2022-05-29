440c744f834ae059ae79110ca98cd21c59ba5ee9
Arkadiusz Świętnicki <nuno69a@gmail.com>
2022-05-29 21:39:06 +0200

    Initial Commit

---
+++ Unitify/UnitifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitifyApp.swift
+//  Unitify
+//
+//  Created by Arkadiusz Świętnicki on 29/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct UnitifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
